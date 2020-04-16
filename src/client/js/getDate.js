b5aade4cedfdd88a11cdc998d88380da04a53e80
Gilps <48761839+1958gilps@users.noreply.github.com>
2020-04-15 20:15:18 -0400

    Add files via upload

--- src/client/js/getDate.js
+++ src/client/js/getDate.js
@@ -2,3 +2,17 @@
 Read
 the date entered on the web page
 */
+function readDate(event) {
+    event.preventDefault();
+
+    // check what date was put into the form field
+    let enterDate = document.getElementById('date').value
+    console.log(enterDate);
+
+    //let enterlenDate = enterDate.length;
+    //console.log(enterLength);
+
+    //Client.checkForDate(enterDate);
+}
+
+export { readDate };    
