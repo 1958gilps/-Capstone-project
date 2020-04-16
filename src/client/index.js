cf7d3fee93aac20e5c873d3ca1d82ca41d05ea1b
Gilps <48761839+1958gilps@users.noreply.github.com>
2020-04-15 20:12:34 -0400

    Add files via upload

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,19 @@
+
+import {readLocation} from './js/getLocation'
+
+import {readDate} from './js/getDate'
 //import dotenv from 'dotenv'
 //dotenv.config()
 
-import './styles/resets.scss'
+
 import './styles/base.scss'
-import './styles/form.scss'
+import './styles/box.scss'
+import './styles/container.scss'
+
 import './styles/footer.scss'
+import './styles/form.scss'
 import './styles/header.scss'
+
+import './styles/resets.scss'
+
+export {readLocation, readDate};
