dfa1c77592fd23b15b98ab9f4ac2c564c418ea57
Gilps <48761839+1958gilps@users.noreply.github.com>
2020-04-15 20:50:25 -0400

    Update getDate.js

--- src/client/js/getDate.js
+++ src/client/js/getDate.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 Read
 the date entered on the web page
@@ -15,24 +14,4 @@ function readDate(event) {
 
     //Client.checkForDate(enterDate);
 }
-
-=======
-/*
-Read
-the date entered on the web page
-*/
-function readDate(event) {
-    event.preventDefault();
-
-    // check what date was put into the form field
-    let enterDate = document.getElementById('date').value
-    console.log(enterDate);
-
-    //let enterlenDate = enterDate.length;
-    //console.log(enterLength);
-
-    //Client.checkForDate(enterDate);
-}
-
->>>>>>> b5aade4cedfdd88a11cdc998d88380da04a53e80
-export { readDate };    
+export { readDate };
