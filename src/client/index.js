fc95b0b11b4a5c886df7c822efdb0dc87527b37b
1958gilps <48761839+1958gilps@users.noreply.github.com>
2020-04-25 11:50:07 -0400

    Lunch break

--- src/client/index.js
+++ src/client/index.js
@@ -2,9 +2,9 @@
 //Event listener (s)
 //document.getElementById('submit').addEventListener('click', apiLocation); //
 document.getElementById('submit').addEventListener('click', apiWeather); //
-document.getElementById('submit').addEventListener('click', apiPicture); //
+//document.getElementById('submit').addEventListener('click', apiPicture); //
 
-import {apiPicture} from './js/getPixabay'
+//import {apiPicture} from './js/getPixabay'
 import {apiWeather} from './js/getWeatherbit'
 //import {apiLocation} from './js/getGeonames' // almost working. Pulls data
 
--- src/client/js/getWeatherbit.js
+++ src/client/js/getWeatherbit.js
@@ -37,50 +37,89 @@ Price: Free
 const baseURL = 'https://api.weatherbit.io/v2.0/forecast/daily?city=';  //16 day weather
 const key = '&key=fc643df8afa84232810d91605c97db23';
 const tempInd = '&units=I'
-//const baseURL = 'https://api.weatherbit.io/v2.0/history/daily?city=';
+const baseURL_past = 'https://api.weatherbit.io/v2.0/history/daily?city=';
 //const historyDate = '&start_date=2019-09-20&end_date=2019-09-21'
 
 export function apiWeather (e) {
     const inputCity = document.getElementById('city').value; // reads the city entered
     const departDate = document.getElementById('departDate').value; // reads the depart date entered
     const returnDate = document.getElementById('returnDate').value; // reads the depart date entered
-    //console.log("city",inputCity);
-    //console.log("departDate", departDate);
-    //console.log("returnDate", returnDate);
-    getWeather(`${baseURL}${inputCity}${key}${tempInd}`) // jumps to getWeather
 
-    .then(function(weatherData) { //DATA as JSON
-        console.log("Weatherbit line # 53", weatherData);
-        let startDayData;
+    let d = new Date();
+    console.log(d);
 
-        for (let dayData of weatherData.data) {
-            //console.log(dayData.valid_date);
-            //console.log(departDate);
-            if (departDate==dayData.valid_date){
-                startDayData=dayData
-                break;
+    if(departDate < d+15) {
+        getWeather_current(`${baseURL}${inputCity}${key}${tempInd}`) // jumps to getWeather_current
+
+        .then(function(weatherData) { //DATA as JSON
+            console.log("Weatherbit line # 53", weatherData);
+            let startDayData;
+    
+            for (let dayData of weatherData.data) {
+                //console.log(dayData.valid_date);
+                //console.log(departDate);
+                if (departDate==dayData.valid_date){
+                    startDayData=dayData
+                    break;
+                }
+            };
+            //console.log(startDayData);
+    
+            const weatherSave = {
+              city_name:weatherData.city_name,
+              state_code:weatherData.state_code,
+              weather:startDayData.weather.description,
+              valid_date:startDayData.valid_date,
+              high_temp:startDayData.high_temp,
+              wind_spd:startDayData.wind_spd,
             }
-        };
-        //console.log(startDayData);
 
+        postData('/saveWeather',(weatherSave)) // jumps to postData
+  
+        .then(
+          updateUI_dateCurrent()
+          )
+      });
+
+    }
+    else {
+        getWeather_past(`${baseURL_past}${inputCity}&start_date={key}${departDate}&end_date=${departDate+1}${key}${tempInd}`) // jumps to getWeather_past
         const weatherSave = {
-          city_name:weatherData.city_name,
-          state_code:weatherData.state_code,
-          weather:startDayData.weather.description,
-          valid_date:startDayData.valid_date,
-          high_temp:startDayData.high_temp,
-          wind_spd:startDayData.wind_spd,
-        }
+            city_name:weatherData.city_name,
+            state_code:weatherData.state_code,
+            datetime:weatherData.data[0].datetime,
+            max_temp:weatherData.data[0].max_temp,
+            min_temp:weatherData.data[0].min_temp,  
+            max_wind_spd:weatherData.data[0].max_wind_spd,
+            snow:weatherData.data[0].snow,
+          }
+
       postData('/saveWeather',(weatherSave)) // jumps to postData
 
       .then(
-        updateUI()
+        updateUI_datePast()
         )
-    });
+    }
 };
 
 //GET async
-const getWeather = async (url) =>{
+const getWeather_current = async (url) =>{
+
+    //console.log(url);
+ 
+    const response = await fetch (url);
+
+    try {
+        const weatherData = response.json();
+        return weatherData;
+
+    } catch (error) {
+        console.log('There is an error in the GET update...'+ error);
+    };
+};
+
+//GET async
+const getWeather_past = async (url) =>{
 
     //console.log(url);
  
@@ -118,7 +157,7 @@ const postData = async function ( url='',data = {}) {
 };
 
 //update UI
-const updateUI = async () => {
+const updateUI_dateCurrent = async () => {
     const request = await fetch ('/weather') // 
         try{
         const serverData = await request.json()
@@ -135,3 +174,25 @@ const updateUI = async () => {
         console.log('There is an error in the UI update...'+ error);
     };
 };
+
+
+//update UI with Historical Data
+const updateUI_datePast = async () => {
+    const request = await fetch ('/weather') // 
+        try{
+        const serverData = await request.json()
+        //console.log("updateUI_datePast",serverData);        
+            document.getElementById('city_name').innerHTML = serverData[0].city_name;
+            document.getElementById('state_code').innerHTML = serverData[0].state_code;
+            document.getElementById('date').innerHTML = serverData[0].datetime;
+            document.getElementById('hi_temp').innerHTML = serverData[0].max_temp;
+            document.getElementById('lo_temp').innerHTML = serverData[0].min_temp;
+            document.getElementById('max_wind_spd').innerHTML = serverData[0].max_wind_spd;
+            document.getElementById('snow_fall').innerHTML = serverData[0].snow;
+            document.getElementById('depart_date').innerHTML = departDate.value;
+            document.getElementById('return_date').innerHTML = returnDate.value;
+    }catch (error){
+        console.log('There is an error in the UI update...'+ error);
+    };
+};
+
