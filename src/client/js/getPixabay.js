9c59c4a4f34e219f72fd85488c1646f679624ac4
1958gilps <48761839+1958gilps@users.noreply.github.com>
2020-04-26 07:45:10 -0400

    4-26-2020 7:45 am

--- src/client/js/getPixabay.js
+++ src/client/js/getPixabay.js
@@ -16,100 +16,31 @@ example - https://pixabay.com/api/?key=16105764-8b11a6d77c5013b870691f430&q=yell
 */
 // "https://pixabay.com/api/?key=16105764-8b11a6d77c5013b870691f430&q=Baltimore, MD&image_type=photo&pretty=true"
 
-const key = '16105764-8b11a6d77c5013b870691f430';
 const baseURL = 'https://pixabay.com/api/';
-
-const example = 'baltimore,md&image_type=photo'
-
-// no good - https://pixabay.com/api/?key=16105764-8b11a6d77c5013b870691f430&q=Baltimore,MD&image_type=photo
-// no good - https://pixabay.com/api/?key=16105764-8b11a6d77c5013b870691f430&q=baltimore,md&image_type=photo
-// is good - https://pixabay.com/api/?key=16105764-8b11a6d77c5013b870691f430&q=Boston,MA&image_type=photo
-// no good - https://pixabay.com/api/?key=16105764-8b11a6d77c5013b870691f430&q=Chicago,IL&image_type=photo
-// is good - https://pixabay.com/api/?key=16105764-8b11a6d77c5013b870691f430&q=Dallas,TX&image_type=photo
-// is good - https://pixabay.com/api/?key=16105764-8b11a6d77c5013b870691f430&q=Miami,FL&image_type=photo
-// is good - https://pixabay.com/api/?key=16105764-8b11a6d77c5013b870691f430&q=Nashville,TN&image_type=photo
-// no good - https://pixabay.com/api/?key=16105764-8b11a6d77c5013b870691f430&q=Pittsburg,PA&image_type=photo
+const key = '16105764-8b11a6d77c5013b870691f430';
 
 export function apiPicture (e) {
     const inputCity = document.getElementById('city').value; // reads the city entered
-    //console.log("city",inputCity);
+
     getPicture(`${baseURL}?key=${key}&q=${inputCity}&image_type=photo`) // jumps to getPicture
 
-    //getPicture(`${baseURL}?key=${key}&q=${example}`) // jumps to getPicture
-
     .then(function(imageData) { //DATA as JSON
-        console.log("pixabay line # 41", imageData);
 
         document.getElementById('image').src = imageData.hits[0].webformatURL;
-
-
-
-
-        //const pictureSave = {
-        //    pageURL:imageData.hits.pageURL,
-        //    tags:imageData.hits.tags,
-        //  }
-
-        //postData('/savePicture',(imageData)) // jumps to postData
-
-        //.then(
-            //console.log("end apiPicture")
-           // updateUI()
-           // )
-        })
-    };
-
+    })
+};
 
 //GET async
 const getPicture= async (url) =>{
 
-    //console.log(url);
- 
     const response = await fetch (url);
 
     try {
         const imageData = response.json();
-        //console.log(imageData );
+
         return imageData ;
-
-    } catch (error) {
-        console.log('There is an error in the GET update...'+ error);
-    };
+        } 
+        catch (error) {
+            console.log('There is an error in the GET update...'+ error);
+        };
 };
-
-/*
-///POST async
-const postData = async function ( url='',data = {}) { 
-    const res = await fetch (url, {  
-        method:'POST',
-        credentials:'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
-
-    try {
-        const newData = res.json();
-
-        //console.log(newData);
-        return newData;
-    }catch (error){
-        console.log('There is an error in the POST update...'+ error);
-    };
-};
-
-//update UI
-const updateUI = async () => {
-    const request = await fetch ('/picture') // 
-        try{
-        const serverData = await request.json()
-        console.log("pixabay line # 102",serverData);
-            
-            document.getElementById('image').innerHTML = serverData.tags;
-
-    }catch (error){
-        console.log('There is an error in the getPixabay UI update...'+ error);
-    };
-};
-*/
